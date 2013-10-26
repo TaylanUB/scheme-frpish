f6a9396089736f9c0cea69266c182ae25b5649f5
Taylan Ulrich B <taylanbayirli@gmail.com>
2013-10-26 14:16:19 +0200

    First commit; first sketch of implementation plan in README.

---
+++ base.scm
@@ -0,0 +1,29 @@
+;;; FRPish --- Functional reactive programming alike framework
+
+;; Copyright (C) 2013  Taylan Ulrich Bayırlı/Kammer
+
+;; Author: Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
+;; Keywords: extensions frp functional reactive
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+;;; base.scm ends here
